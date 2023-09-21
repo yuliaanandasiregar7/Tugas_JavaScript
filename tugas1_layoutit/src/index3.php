46f81c8fbe4c3e253b0c8428b34e70f3dc7cbb2a
yuliaanandasiregar7 <yuli21073ti@student.nurulfikri.ac.id>
2023-09-21 16:03:06 +0700

    tugas1php

---
+++ tugas1_layoutit/src/index3.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Data Pribadi</title>
+
+    <meta
+      name="description"
+      content="Source code generated using layoutit.com"
+    />
+    <meta name="author" content="LayoutIt!" />
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="carousel slide" id="carousel-508782">
+          <ol class="carousel-indicators">
+            <li data-slide-to="0" data-target="#carousel-508782"></li>
+            <li data-slide-to="1" data-target="#carousel-508782"></li>
+            <li
+              data-slide-to="2"
+              data-target="#carousel-508782"
+              class="active"
+            ></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img
+                class="d-block w-80"
+                alt="Carousel Bootstrap First"
+                src="image/WhatsApp Image 2023-03-15 at 10.08.01.jpeg"
+              />
+              <div class="carousel-caption">
+                <h4>Assalamualaikum</h4>
+                <p>
+                  Perkenalkan Namaku Yulia Ananda Siregar Asalku Dari
+                  Padangsidimpuan
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-80"
+                alt="Carousel Bootstrap Second"
+                src="image/WhatsApp Image 2023-03-15 at 10.08.01.jpeg"
+              />
+              <div class="carousel-caption">
+                <h4>Mahasiswa</h4>
+                <p>
+                  Aku Adalah Mahasiwa Sekolah Tinggi Teknologi Terpadu Nurul
+                  Fikri
+                </p>
+              </div>
+            </div>
+            <div class="carousel-item active">
+              <img
+                class="d-block w-80"
+                alt="Carousel Bootstrap Third"
+                src="image/foto6.jpeg"
+              />
+              <div class="carousel-caption">
+                <h4>Jurusan</h4>
+                <p>Aku Berkuliah Jurusan Teknik Informatika</p>
+              </div>
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carousel-508782"
+            data-slide="prev"
+            ><span class="carousel-control-prev-icon"></span>
+            <span class="sr-only">Previous</span></a
+          >
+          <a
+            class="carousel-control-next"
+            href="#carousel-508782"
+            data-slide="next"
+            ><span class="carousel-control-next-icon"></span>
+            <span class="sr-only">Next</span></a
+          >
+        </div>
+      </div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
