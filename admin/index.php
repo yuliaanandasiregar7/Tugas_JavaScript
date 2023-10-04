d993fcb969841c2005e9e1a0f101a3e8216ddfbb
yuliaanandasiregar7 <yuli21073ti@student.nurulfikri.ac.id>
2023-10-04 11:36:38 +0700

    lanjutan webnative

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+
+?>
+<div>
+    <div class="container-fluid px-4">
+        <!--<h1>Selamat Datang Di Halaman Admin</h1> -->
+
+        <?php
+        error_reporting(0);
+        //membuat logic sederhana untuk mengarahkan url ke file
+        //yang mempunyai extension.php
+        //routing => file yang mengarahkan ke url
+
+        $url = $_GET['url'];
+        if($url == 'dashboard'){
+            include_once 'dashboard.php';
+        }else if (!empty($url)){
+            include_once ''.$url.'.php';
+        }else {
+          include_once 'dashboard.php';
+        }
+        ?>
+    </div>
+</div>
+
+<?php
+include_once 'bottom.php';
+?>
